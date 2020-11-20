2e56d4df5d8d786bbd46fa70e50301e9aec0dfe9
Semih Aslan <aslan@txstate.edu>
2020-11-19 18:26:18 -0600

    DFF added

---
+++ PartI/DFF_tb.v
@@ -0,0 +1,25 @@
+`include "DFF_SR.v"
+module tb_DFF();
+reg D;
+reg clk;
+reg reset;
+wire Q;
+
+DFF_SR dut(D,clk,reset,Q);
+
+initial begin
+  clk=0;
+     forever #10 clk = ~clk;  
+end 
+initial begin 
+ reset=1;
+ D <= 0;
+ #100;
+ reset=0;
+ D <= 1;
+ #100;
+ D <= 0;
+ #100;
+ D <= 1;
+end 
+endmodule 
